73a10c85fdfbd2154b4b455eb8590546deaf16e0
Ashwini Chougule <ashwinic@mirrorar.io>
2021-02-02 14:10:28 +0530

    API to oder pizza

---
+++ PizzaAPI/Controllers/PizzaController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PizzaAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PizzaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        // GET: api/<PizzaController>
+        [HttpGet]
+        [Route("BasePizza")]
+        public BasePizzaList GetBasePizza()
+        {
+            var basePizzas = new BasePizzaList();
+            using (StreamReader r = new StreamReader("Data/basePizza.json"))
+            {
+                string json = r.ReadToEnd();
+                basePizzas = JsonConvert.DeserializeObject<BasePizzaList>(json);
+            }
+
+            return basePizzas;
+        }
+
+        [HttpGet]
+        [Route("Toppings")]
+        public List<string> GetToppings()
+        {
+            var items = new List<String> { "Tomato", "Cheese", "Onion", "Paneer", "Capsicum", "Mushroom", "Jalapeno", "Corn" };
+            return items;
+        }
+
+        [HttpGet]
+        [Route("Sizes")]
+        public List<string> GetSize()
+        {
+            var items = new List<String> { "Small", "Medium", "Large" };
+            return items;
+        }
+
+        [HttpGet]
+        [Route("Crust")]
+        public List<string> GetCrust()
+        {
+            var items = new List<String> { "Hand Tossed", "Cheese Bust", "Thin Crust" };
+            return items;
+        }
+
+        [HttpGet]
+        [Route("Sauce")]
+        public List<string> GetSauce()
+        {
+            var items = new List<String> { "Musturd", "Mozzerilla", "Tomato" };
+            return items;
+        }
+
+        // GET api/<PizzaController>/5
+        [HttpGet("{id}")]
+        public BasePizza Get(int id)
+        {
+            var basePizzas = new BasePizzaList();
+            using (StreamReader r = new StreamReader("Data/basePizza.json"))
+            {
+                string json = r.ReadToEnd();
+                basePizzas = JsonConvert.DeserializeObject<BasePizzaList>(json);
+            }
+            var selectedPizza = basePizzas.basePizzas.Where(x => x.id == id).FirstOrDefault();
+
+            return selectedPizza;
+        }
+
+        // POST api/<PizzaController>
+        [HttpPost]
+        public void Post([FromBody] Order orderItem)
+        {
+            var orders = new OrderList();
+            using (StreamReader r = new StreamReader("Data/order.json"))
+            {
+                string json = r.ReadToEnd();
+                orders = JsonConvert.DeserializeObject<OrderList>(json);
+                orderItem.orderId = orders.order.Count() + 1;
+                orders.order.Add(orderItem);
+            }           
+            var jsonData = JsonConvert.SerializeObject(orders);
+            using (StreamWriter w = new StreamWriter("Data/order.json"))
+            {
+                w.Write(jsonData);
+            }
+
+        }
+        [HttpGet]
+        [Route("Orders")]
+        public OrderList GetOrderList()
+        {
+            var orders = new OrderList();
+            using (StreamReader r = new StreamReader("Data/order.json"))
+            {
+                string json = r.ReadToEnd();
+                orders = JsonConvert.DeserializeObject<OrderList>(json);
+            }
+
+            return orders;
+        }
+
+        // PUT api/<PizzaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PizzaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PizzaAPI/Models/Pizza.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaAPI.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class BasePizza
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<string> ingredients { get; set; }
+        public int basePrice { get; set; }
+    }
+
+    public class BasePizzaList
+    {
+        public List<BasePizza> basePizzas { get; set; }
+    }
+
+    public class Pizza
+    {
+        public BasePizza basePizza { get; set; }
+        public List<string> toppings { get; set; }
+        public List<string> sauce { get; set; }
+        public string size { get; set; }
+        public string crust { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+    }
+
+    public class Order
+    {
+        public List<Pizza> pizzas { get; set; }
+        public int orderId { get; set; }
+        public int bill { get; set; }
+    }
+
+    public class OrderList
+    {
+        public List<Order> order { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+}
