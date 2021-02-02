9d56115460b725ddab437b8d699e528e7860096a
Ashwini Chougule <ashwinic@mirrorar.io>
2021-02-02 14:10:00 +0530

    API to order pizza

---
+++ OrderPizzaAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrderPizzaAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
